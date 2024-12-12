d770c03b1b90b4d7ce061fe10db30167f8d36d84
tcaothien <116654835+tcaothien@users.noreply.github.com>
2024-12-12 22:45:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const { mongoURI, token } = require('./config/config');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 // Kết nối MongoDB
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Đã kết nối MongoDB"))
   .catch(err => console.log(err));
 
 // Khi bot đã sẵn sàng
